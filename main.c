c9f6573ef079fbcb97655307fd23941a33d56838
lnbanuelos1 <70293313+lnbanuelos1@users.noreply.github.com>
2020-09-30 11:58:10 -0600

    Update main.c

--- main.c
+++ main.c
@@ -16,22 +16,22 @@ int main(void) {
   scanf("%d %d", &s, &d);
   
  if(s < d){
-   printf("Error");
+   printf("Error");                                                       //if s<d then error
  }
  else if((s < 120) && (d < 80)) {
-   printf("Normal\n");
+   printf("Normal\n");                                                    //Reading for  Normal BP
   }
  else if((120 <= s && s <= 129) && (d < 80)) {
-   printf("Elevated\n");
+   printf("Elevated\n");                                                  //Reading for Elevated BP
   }
  else if((130 <= s && s <= 139) || (80 <= d && d <= 89)) {
-   printf("Stage 1 Hypertension\n");
+   printf("Stage 1 Hypertension\n");                                      //Reading for Stage 1 Hypertension
   }
  else if((140 <= s && s <= 179) || (90 <= d && d <= 119 )) {
-   printf("Stage 2 Hypertension\n");
+   printf("Stage 2 Hypertension\n");                                      //Reading for Stage 2 Hypertension
   }
  else if((s > 180) || (d > 120)) {
-   printf("Hypertensive Crysis\n");
+   printf("Hypertensive Crysis\n");                                       //Reading for Hypertensive Crysis 
   }
   return 0;
 }
