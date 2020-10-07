c42d1423cf68160e9a1ad4eccb16159d769faf6a
lnbanuelos1 <lnbanuelos1@miners.utep.edu>
2020-10-07 00:58:19 +0000

    Done earlier but did not commit and push

--- main.c
+++ main.c
@@ -2,9 +2,6 @@
 *   CODING CHALLENGE #2
 *   Filename: main.c
 *   This file measures a person's blood pressure readings and reports their category.
-*   Last modified by: Angel F. Garcia (9/19/2020)
-*   
-*   Check the // TODO comments to see what is missing and needs to be completed.
 */
 
 #include <stdio.h>
